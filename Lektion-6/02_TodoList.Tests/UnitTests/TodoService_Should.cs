46ac065d131119da88918868e1c8997acb827689
chris <christofferjcm@gmail.com>
2023-09-19 21:18:16 +0200

    create TodoList and TodoList.Tests

---
+++ Lektion-6/02_TodoList.Tests/UnitTests/TodoService_Should.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_TodoList.Tests.UnitTests;
+
+public class TodoService_Should
+{
+
+}
